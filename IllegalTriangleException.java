0162cd2930ce17c2a3f24ee4fbb5793dd18c2764
Hoan Nguyen <nguyenhoan.hut@gmail.com>
2016-07-25 02:12:23 +0700

    Custom exception

---
+++ IllegalTriangleException.java
@@ -0,0 +1,8 @@
+package exceptionHandling_ex5;
+
+public class IllegalTriangleException extends RuntimeException {
+
+	public IllegalTriangleException(String string) {
+		super(string);
+	}
+}
---
+++ Main.java
@@ -0,0 +1,30 @@
+package exceptionHandling_ex5;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		while (true) {
+			try {
+				Scanner input = new Scanner(System.in);
+				System.out.println("Enter side1 please:");
+				int side1 = input.nextInt();
+				System.out.println("Enter side2:");
+				int side2 = input.nextInt();
+				System.out.println("Enter side3:");
+				int side3 = input.nextInt();
+
+				Triangle triangle = new Triangle(side1, side2, side3);
+				if (!(side1 + side2 < side3) || (side1 + side3 < side2)
+						|| (side2 + side3 < side1)) {
+					System.out.println("OK, these are 3 sides of a triangle");
+					break;
+				}
+			} catch (IllegalTriangleException ex) {
+				System.out.println("IllegalTriangleException, try again");
+			}
+		}
+	}
+
+}
